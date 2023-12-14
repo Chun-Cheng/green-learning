fb7f5ad450b132d5b797f53363f6080509104ce6
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2023-12-14 09:55:32 +0800

    Refactor: Change port

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ async def websites(request: Request):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
+    uvicorn.run('main:app', host='127.0.0.1', port=80, reload=True)
 
 # Document: 
 # https://fastapi.tiangolo.com/tutorial/first-steps/
