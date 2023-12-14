f2d4d65b54322a6d6c30028d5aecdd7bd0294c74
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2023-12-14 20:23:38 +0800

    Feat: Create book page router and page

---
+++ routers/get_book_page.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+import re
+import markdown
+from model import model
+
+router = APIRouter()
+templates = Jinja2Templates(directory='templates')
+
+# book page
+@router.get('/book/{book_id}/{page_id}', response_class=HTMLResponse)
+async def root(request: Request, book_id: str, page_id: str):
+    # check input data => regex
+    book_id = re.search('[a-z0-9_-]+', book_id).string
+    page_id = re.search('[a-z0-9_-]+', page_id).string
+
+    # get book data
+    book = model.execute('SELECT url, title, pages FROM books WHERE url=?', (book_id,)).fetchone()
+    # book not found
+    if book is None:
+        return RedirectResponse('/404')
+
+    # get page data
+    page = model.execute('SELECT title, author, last_update, tags, content FROM pages WHERE url=?', (page_id,)).fetchone()
+    # page not found
+    if page is None:
+        return RedirectResponse('/404')
+
+    # organize and convert data
+    # book
+    book_url = book[0]
+    book_title = book[1]
+    book_pages = book[2].split(',')
+    # page
+    page_title = page[0]
+    page_author = page[1]
+    page_last_update = page[2]
+    page_tags = page[3].split(',')
+    page_content = markdown.markdown(page[4])  # markdown to html
+
+    # return
+    return templates.TemplateResponse('book_page.html', {'request': request, 
+                                                         'book_title': book_title, 
+                                                         'page_title': page_title,
+                                                         'page_author': page_author,
+                                                         'page_last_update': page_last_update,
+                                                         'page_tags': page_tags,
+                                                         'page_content': page_content})
