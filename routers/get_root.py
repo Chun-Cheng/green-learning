0e0855a44ac1f8dde724ba9f9336371470d4e737
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2023-12-29 14:07:57 +0800

    Feat: Update homepage and book-page

--- routers/get_root.py
+++ routers/get_root.py
@@ -9,13 +9,14 @@ templates = Jinja2Templates(directory='templates')
 # homepage
 @router.get('/', response_class=HTMLResponse)
 async def homepage(request: Request):
-    articles = model.execute('SELECT id, title, author, update_datetime, content FROM pages').fetchall()  # Where book is None/Null/undefined
+    articles = model.execute('SELECT id, title, author, update_datetime, content FROM pages WHERE book_id = \'\'').fetchall()  # Where book is None/Null/undefined
     articles = map(lambda r: {
                    'id': r[0],
                    'title': r[1],
                    'author': r[2],
                    'update_datetime': r[3],
                    'content': r[4]}, articles)
+    # TODO: articles are not displayed
     topics = model.execute('SELECT id, title, description, pages FROM books').fetchall()
     topics = map(lambda r: {'id': r[0],
                             'title': r[1],
