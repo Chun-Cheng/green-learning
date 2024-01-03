b54a1e97fbe38e815c1a2ee517d52726b8afd846
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2024-01-03 16:45:17 +0800

    Feat: Jump sign in function

--- routers/get_signin.py
+++ routers/get_signin.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
@@ -8,4 +8,7 @@ templates = Jinja2Templates(directory='templates')
 # sign in page
 @router.get('/signin', response_class=HTMLResponse)
 async def sign_in_page(request: Request):
+    # has signed in
+    return RedirectResponse('/dashboard')
+    # not signed in
     return templates.TemplateResponse('signin.html', {'request': request})
--- routers/get_signup.py
+++ routers/get_signup.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
@@ -8,4 +8,7 @@ templates = Jinja2Templates(directory='templates')
 # sign up page
 @router.get('/signup', response_class=HTMLResponse)
 async def sign_up_page(request: Request):
+    # has signed in
+    return RedirectResponse('/dashboard')
+    # not signed in
     return templates.TemplateResponse('signup.html', {'request': request})
