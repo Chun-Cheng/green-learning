ee733612debb8a958ac2d1b06503f7b47b7ec83d
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2024-01-13 11:05:44 +0800

    Feat: Read record

--- routers/get_api_add_read_time.py
+++ routers/get_api_add_read_time.py
@@ -9,28 +9,31 @@ router = APIRouter(prefix='/api', tags=['APIs'])
 templates = Jinja2Templates(directory='templates')
 
 # sign in api
-@router.post('/add_read_time')
-async def add_read_time(request: Request, user: str, page_id: str, seconds: int, update: str):
+@router.get('/add_read_time')
+async def add_read_time(request: Request, session_id: str, page_id: str, seconds: int, update: str):
     # check params's format (regex)
 
-    # check whether the account exist
-    # if not exist
-    #     raise HTTPException()
+    # get user by session_id
+    user = model.execute('SELECT username from sessions WHERE id = ? ', (session_id,)).fetchone()
+    if user is None:
+        raise HTTPException(404)
+    username = user[0]
+    print(username)  #
 
-    reading_record = model.execute('SELECT duration from reading_history WHERE username = ? AND page_id = ?', f'%{user}%', f'%{page_id}%')
+    reading_record = model.execute('SELECT duration from reading_history WHERE username = ? AND page_id = ?', (user, page_id)).fetchone()
     if reading_record is None:
-        new_reading_record = [
-            f'{user}-{page_id}',  # id
+        new_reading_record = (
+            f'{username}-{page_id}',  # id
             user,  # username
             page_id,  
             update,  # start_datetime
             update,  # update_datetime
             seconds  # duration
-        ]
+        )
         model.execute('INSERT INTO reading_history VALUES(?, ?, ?, ?, ?, ?)', new_reading_record)
     else:
         seconds += reading_record[0]
-        model.execute('UPDATE reading_history SET duration = ?, update_datetime = ? WHERE username = ? AND page_id = ?', seconds, update)
+        model.execute('UPDATE reading_history SET duration = ?, update_datetime = ? WHERE username = ? AND page_id = ?', (seconds, update))
     
     response_content = {}
     response_content = jsonable_encoder(response_content)
--- static/js/page.js
+++ static/js/page.js
@@ -5,8 +5,33 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 
 // read record
 window.addEventListener('load', (event) => {
-    let record_timer_id = setInterval(() => {
-        // record here
-        console.log('record');
-    }, 5000);
+    let record_timer_id = setInterval(async () => {
+        let session_id = window.localStorage.getItem('session_id');
+        console.debug(session_id)
+        if(session_id === null) {
+            return;
+        }
+        let splitted_url = window.location.href.split('/');
+        let page_id = splitted_url[splitted_url.length-1];
+        let update_datetime = now_iso();
+        // get /add_read_time session_id page_id seconds(3) update
+        await fetch(`/api/add_read_time?session_id=${session_id}&page_id=${page_id}&seconds=3&update=${update_datetime}`)  // , {method: 'GET'}
+            .then(response => {
+                console.debug(`/api/add_read_time 3: ${response.status}`);
+            });
+        console.log('reading time recorded');
+    }, 3000);  //
 });
+
+// get now ISO string
+function now_iso() {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() >= 10 ? `${now.getMonth()}` : `0${now.getMonth()}`;
+    let date = now.getDate() >= 10 ? `${now.getDate()}` : `0${now.getDate()}`;
+    let hours = now.getHours() >= 10 ? `${now.getHours()}` : `0${now.getHours()}`;
+    let minutes = now.getMinutes() >= 10 ? `${now.getMinutes()}` : `0${now.getMinutes()}`;
+    let seconds = now.getSeconds() >= 10 ? `${now.getSeconds()}` : `0${now.getSeconds()}`;
+    let milliseconds = now.getMilliseconds();
+    return `${year}-${month}-${date}T${hours}:${minutes}:${seconds}.${milliseconds}000`;
+}
