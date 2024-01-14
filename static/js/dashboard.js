d0011e201a720e3a559a9c8bddaf12ac2e5b3b1b
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2024-01-15 00:20:10 +0800

    Feat: Dashboard auto redirect
    Redirect to sign-in page automatically if the user hasn't signed in.

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,4 +1,22 @@
-// detect dark mode
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.setAttribute('data-bs-theme', 'dark');
-}
+// onload
+window.addEventListener('load', async (event) => {
+    // has signed in
+    let session_id = window.localStorage.getItem('session_id');
+    // check whether the session_id valid
+    fetch(`/api/check_session?session_id=${session_id}`)
+        .then(response => {
+            if (!response.ok) {
+                // not ok
+                window.localStorage.removeItem('session_id');
+                window.location.replace('/signin');
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    // detect dark mode
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+    }
+});
